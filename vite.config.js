19186c6ac441c300955100606dd3622a38f32f26
milindSaugat <milindsaugat1122@gmail.com>
2026-09-22 11:00:50 +0530

    Configure Vite server to run locally on single localhost port

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: true,
     watch: {
       ignored: ['**/*.postman_collection*.json', '**/node_modules/**', '**/dist/**']
     }
